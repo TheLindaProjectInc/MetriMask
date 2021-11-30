9f0757e61adaee70ad47b0504633aa2abb3272f6
nibbles83 <hardke01@yahoo.co.uk>
2021-11-30 13:00:49 +0000

    update token polling

--- src/background/controllers/sessionController.ts
+++ src/background/controllers/sessionController.ts
@@ -41,13 +41,13 @@ export default class SessionController extends IController {
   public clearSession = () => {
     this.main.account.resetAccount();
     this.main.token.resetTokenList();
-    this.main.inpageAccount.sendInpageAccountAllPorts(
-      METRIMASK_ACCOUNT_CHANGE.LOGOUT
-    );
+    this.main.mrc721Token.resetTokenList();
+    this.main.inpageAccount.sendInpageAccountAllPorts(METRIMASK_ACCOUNT_CHANGE.LOGOUT);
   }
 
   private clearAllIntervalsExceptAccount = () => {
     this.main.token.stopPolling();
+    this.main.mrc721Token.stopPolling();
     this.main.external.stopPolling();
     this.main.transaction.stopPolling();
   }

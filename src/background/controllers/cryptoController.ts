646099d3ab610e8ddab95df7545e55438ee43609
nibbles83 <hardke01@yahoo.co.uk>
2022-09-30 13:07:01 +0100

    Remove old scriptworker worker item

--- src/background/controllers/cryptoController.ts
+++ src/background/controllers/cryptoController.ts
@@ -69,8 +69,6 @@ export default class CryptoController extends IController {
       throw Error('appSalt should not be empty');
     }
 
-
-    // FIXME this causes the loading screen to "freeze"
     try {
       const saltBuffer = Buffer.from(this.appSalt);
       const { N, r, p } = CryptoController.SCRYPT_PARAMS_PW;
@@ -81,52 +79,5 @@ export default class CryptoController extends IController {
     } catch (err) {
       console.log({ err });
     }
-
-
-    /*
-    * Create a web worker for the scrypt key derivation, so that it doesn't freeze the loading screen ui.
-    * File path relative to post bundling of webpack. worker-loader node module did not work for me,
-    * possibly a compatibility issue with chrome.
-    */
-    //  let sww;
-    //  if (typeof(sww) === 'undefined') {
-    //     sww = new self.Worker(
-    //     // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    //     self.URL.createObjectURL(new Blob(['('+this.worker_function.toString()+')()'], {type: 'text/javascript'})));
-    //     // sww = new Worker('./scryptworker.js');
-
-    //     sww.postMessage({
-    //      password,
-    //      salt: this.appSalt,
-    //      scryptParams: CryptoController.SCRYPT_PARAMS_PW,
-    //    });
-
-    //    sww.onmessage = (e) => {
-    //      if (e.data.err) {
-    //        throw Error('scrypt failed to calculate derivedKey');
-    //      }
-    //      this.passwordHash = e.data.passwordHash;
-    //      finish();
-    //    };
-    // }
   };
-
-  // public worker_function = () => {
-  //   onmessage = (e) => {
-  //     const ctx: Worker = self as any;
-  //     try {
-  //       const password = e.data.password;
-  //       const salt = e.data.salt;
-  //       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  //       const saltBuffer = Buffer.from(salt);
-  //       const { N, r, p } = e.data.scryptParams;
-  //       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  //       const derivedKey = scrypt(password, saltBuffer, N, r, p, 64);
-  //       const passwordHash = derivedKey.toString('hex');
-  //       ctx.postMessage({ passwordHash });
-  //     } catch (err) {
-  //       ctx.postMessage({ err });
-  //     }
-  //   };
-  // };
 }

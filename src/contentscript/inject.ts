6b89b0d0e9128b1febcdad94f36ccde4d479d46f
nibbles83 <hardke01@yahoo.co.uk>
2022-09-30 10:31:48 +0100

    replace deprecated extension with runtime

--- src/contentscript/inject.ts
+++ src/contentscript/inject.ts
@@ -25,29 +25,29 @@ const injectStylesheet = (src: string) => {
 };
 
 export const injectAllScripts = async () => {
-  await injectScript(chrome.extension.getURL('commons.all.js')).then(async () => {
-    await injectScript(chrome.extension.getURL('commons.exclude-background.js'));
-    await injectScript(chrome.extension.getURL('commons.exclude-contentscript.js'));
-    await injectScript(chrome.extension.getURL('commons.exclude-popup.js'));
-    await injectScript(chrome.extension.getURL('commons.background-inpage.js'));
-    await injectScript(chrome.extension.getURL('commons.contentscript-inpage.js'));
-    await injectScript(chrome.extension.getURL('commons.popup-inpage.js'));
-    await injectScript(chrome.extension.getURL('inpage.js'));
+  await injectScript(chrome.runtime.getURL('commons.all.js')).then(async () => {
+    await injectScript(chrome.runtime.getURL('commons.exclude-background.js'));
+    await injectScript(chrome.runtime.getURL('commons.exclude-contentscript.js'));
+    await injectScript(chrome.runtime.getURL('commons.exclude-popup.js'));
+    await injectScript(chrome.runtime.getURL('commons.background-inpage.js'));
+    await injectScript(chrome.runtime.getURL('commons.contentscript-inpage.js'));
+    await injectScript(chrome.runtime.getURL('commons.popup-inpage.js'));
+    await injectScript(chrome.runtime.getURL('inpage.js'));
 
     // Pass the Chrome extension absolute URL of the Sign Transaction dialog to the Inpage
-    const signTxUrl = chrome.extension.getURL('sign-tx.html');
+    const signTxUrl = chrome.runtime.getURL('sign-tx.html');
     postWindowMessage(TARGET_NAME.INPAGE, {
       type: API_TYPE.SIGN_TX_URL_RESOLVED,
       payload: { url: signTxUrl },
     });
 
     // Pass the Chrome extension absolute URL of the Sign Message dialog to the Inpage
-    const signMessageUrl = chrome.extension.getURL('sign-message.html');
+    const signMessageUrl = chrome.runtime.getURL('sign-message.html');
     postWindowMessage(TARGET_NAME.INPAGE, {
       type: API_TYPE.SIGN_MESSAGE_URL_RESOLVED,
       payload: { url: signMessageUrl },
     });
   });
 
-  injectStylesheet(chrome.extension.getURL('css/modal.css'));
+  injectStylesheet(chrome.runtime.getURL('css/modal.css'));
 };

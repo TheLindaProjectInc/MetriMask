d5d36f7d7354989129f5e7859c848c801cba5a01
nibbles83 <hardke01@yahoo.co.uk>
2022-09-30 09:58:34 +0100

    Remove scriptworker, can no longer launch in seperate worker

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -16,7 +16,6 @@ module.exports = {
     contentscript: './src/contentscript/index.ts',
     inpage: './src/inpage/index.ts',
     popup: './src/popup/index.tsx',
-    scryptworker: './src/background/workers/scryptworker.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),

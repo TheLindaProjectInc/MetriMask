bc19cea08f21706487af9eda7bb2d078f3ebc222
nibbles83 <hardke01@yahoo.co.uk>
2024-08-15 16:39:19 +0100

    Fix for V3 servceworker closing

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,9 @@
 import MetriMaskController from './controllers';
 
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
+
 // Add instance to window for debugging
 const controller = new MetriMaskController();
 Object.assign(chrome.windows.getCurrent, { controller });
